6a42a3adbc15f50d51405252614f28c062fbeb76
yuuta2s <asmaa.guebba19@gmail.com>
2024-06-13 13:53:31 +0200

    creation de l'arborescence front end

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,13 +5,14 @@ import Home from "./pages/Home";
 const App = () => {
   return (
     <Router>
-      <Navbar/>
+      <Header/>
       <Routes>
         <Route exact path="/" element={<Homepage/>} />
         <Route path="/discover" element={<TournamentList/>} />
         <Route path="/mes-tournois" element={<TournamentDetail/>} />
         <Route path="/contact" element={<Contact/>} />
       </Routes>
+      <Footer/>
     </Router>
   );
 };
