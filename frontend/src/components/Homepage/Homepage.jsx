b05c7b6f2b1a1cf41e528643094af2d65d4b348b
ElioDKM <eliodikoume@gmail.com>
2024-06-17 17:27:38 +0200

    Amelioration homepage et liaison back et front

--- frontend/src/components/Homepage/Homepage.jsx
+++ frontend/src/components/Homepage/Homepage.jsx
@@ -1,27 +1,43 @@
 import React from 'react';
+import { useEffect, useState } from 'react';
+import axios from 'axios';
 import design2 from '../../assets/Rectangle 261.svg';
 import mkLogo from '../../assets/mkLogo.png'
 
 export default function Homepage() {
 
+  const [tournaments, setTournaments] = useState([]);
 
+  const fetchData = async () => {
+    try {
+      const res = await axios.get("http://localhost:5000/tournament");
+      console.log(res.data);
+      setTournaments(res.data);
+    } catch (error) {
+      console.error("Error fetching data:", error);
+    }
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
 
   return (
-    <div className=" bg-gradient-to-tr from-black via-vertBG to-black p-4">
+    <div className="bg-gradient-to-tr from-black via-vertBG to-black p-4">
       <div className="flex flex-col items-center mb-12">
         <h1 className="text-4xl font-bold text-white">Become an Arena Maven</h1>
         <div className="w-full h-20 flex justify-center absolute top-3.5 right-3.5">
           <img src={design2} alt="Design" />
         </div>
       </div>
-      <section className="flex justify-evenly items-center">
+      <section className="flex justify-evenly items-center bg-black rounded-lg bg-opacity-40 p-3">
         <div className="p-3">
           <h2 className="text-3xl font-bold text-white p-3">Ici c'est que la win !</h2>
           <div className="flex justify-between items-center gap-3">
-            <button class="bg-primary hover:bg-secondary text-white font-bold py-2 px-4 border-b-4 border-secondary hover:border-tertiary rounded">
+            <button className="bg-primary hover:bg-secondary text-white font-bold py-2 px-4 border-b-4 border-secondary hover:border-tertiary rounded">
               Create a tournament
             </button>
-            <button class="bg-primary hover:bg-secondary text-white font-bold py-2 px-4 border-b-4 border-secondary hover:border-tertiary rounded">
+            <button className="bg-primary hover:bg-secondary text-white font-bold py-2 px-4 border-b-4 border-secondary hover:border-tertiary rounded">
               Find a tournament
             </button>
           </div>
@@ -33,19 +49,23 @@ export default function Homepage() {
       <section>
         <article className="mt-8">
           <h2 className="text-2xl text-white">Hottest :</h2>
-          <div className="flex items-center justify-evenly">
-              <p>image 1 tournois</p>{/* <img src="" alt="" /> */}
-              <p>image 2 tournois</p>{/* <img src="" alt="" /> */}
-              <p>image 3 tournois</p>{/* <img src="" alt="" /> */}
-          </div>
+          {tournaments.length > 0 && (
+            <div className="flex flex-wrap items-center justify-evenly">
+              <img className="w-80 rounded-lg" src={tournaments[0].tournament_img} alt="image tournois" />
+              <img className="w-80 rounded-lg" src={tournaments[0].tournament_img} alt="image tournois" />
+              <img className="w-80 rounded-lg" src={tournaments[0].tournament_img} alt="image tournois" />
+            </div>
+          )}
         </article>
         <article className="mt-8">
           <h2 className="text-2xl text-white">Last Chance :</h2>
-          <div className="flex items-center justify-evenly">
-              <p>image 1 tournois</p>{/* <img src="" alt="" /> */}
-              <p>image 2 tournois</p>{/* <img src="" alt="" /> */}
-              <p>image 3 tournois</p>{/* <img src="" alt="" /> */}
-          </div>
+          {tournaments.length > 0 && (
+            <div className="flex flex-wrap items-center justify-evenly">
+              <img className="w-80 rounded-lg" src={tournaments[0].tournament_img} alt="image tournois" />
+              <img className="w-80 rounded-lg" src={tournaments[0].tournament_img} alt="image tournois" />
+              <img className="w-80 rounded-lg" src={tournaments[0].tournament_img} alt="image tournois" />
+            </div>
+          )}
         </article>
       </section>
     </div>
