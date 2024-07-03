8bd9ef0fef8da77f13726575a2f8ecd66277fede
Leaula21 <lola.mounier.lm@gmail.com>
2024-07-03 17:02:29 +0200

    Affichage des anciens tournois avec images

--- frontend/src/components/Profil/Profil.jsx
+++ frontend/src/components/Profil/Profil.jsx
@@ -1,21 +1,22 @@
 import UserPic from '../../assets/UserPic.png';
 import { useEffect, useState } from 'react';
 import axios from 'axios';
-import SmallCards from '@components/SmallCards/SmallCards'; // Assurez-vous que le chemin est correct
+import SmallCards from '@components/SmallCards/SmallCards';
 
 export default function Profil() {
     const [tournaments, setTournaments] = useState([]);
 
     useEffect(() => {
-      const fetchData = async () => {
-        try {
-          const res = await axios.get("http://localhost:5000/tournament");
-          setTournaments(res.data);
-        } catch (error) {
-          console.error("Error fetching data:", error);
-        }
-      };
-      fetchData();
+        const fetchData = async () => {
+            try {
+                const res = await axios.get("http://localhost:5000/tournament");
+                console.log(res.data);
+                setTournaments(res.data);
+            } catch (error) {
+                console.error("Error fetching data:", error);
+            }
+        };
+        fetchData();
     }, []);
 
     return (
@@ -36,20 +37,19 @@ export default function Profil() {
                 </div>
             </div>
 
-            {/* Old tournament section */}
-            {/* Affichage des tournois dynamiquement */}
+            {/* Button section */}
+            <div className="flex justify-center p-4 mb-4">
+                <button className="relative h-[50px] overflow-hidden bg-opacity-0 px-3 text-white border-l-4 border-primary shadow-2xl transition-all before:absolute before:bottom-0 before:left-0 before:top-0 before:z-0 before:h-full before:w-0 before:bg-primary before:transition-all before:duration-500 hover:text-white hover:shadow-[0_0_20px_rgba(0,0,0,0.8),0_0_20px_var(--tw-shadow-color)] hover:shadow-secondary hover:before:left-0 hover:before:w-full">
+                    <span className="relative z-10">Anciens tournois</span>
+                </button>
+            </div>
+
+            {/* New tournament section */}
             <div className="flex flex-col items-center p-4">
                 {/* Tournaments section */}
                 <div className="flex flex-wrap justify-center w-full gap-4">
                     {tournaments.map((tournament, index) => (
-                        <div key={index} className="max-w-sm overflow-hidden shadow-lg w-full sm:w-auto">
-                            <div className="px-6 py-4 bg-secondary">
-                                <div className="font-bold text-xl mb-2">{tournament.name}</div>
-                                <p className="text-gray-700 text-base">
-                                    {tournament.description}
-                                </p>
-                            </div>
-                        </div>
+                        <SmallCards key={tournament.id} tournament={tournament} index={index} />
                     ))}
                 </div>
             </div>
