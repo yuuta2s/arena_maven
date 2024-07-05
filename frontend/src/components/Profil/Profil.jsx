a1bf467a70ea06567b5fb93d6dd92b21b91086b6
Leaula21 <lola.mounier.lm@gmail.com>
2024-07-05 14:43:20 +0200

    Affichage Anciens tournois + Tournois en cours avec filtre

--- frontend/src/components/Profil/Profil.jsx
+++ frontend/src/components/Profil/Profil.jsx
@@ -5,19 +5,57 @@ import SmallCards from '@components/SmallCards/SmallCards';
 
 export default function Profil() {
     const [tournaments, setTournaments] = useState([]);
+    const [participantsData, setParticipantsData] = useState({});
 
     useEffect(() => {
-        const fetchData = async () => {
+        const fetchTournaments = async () => {
             try {
                 const res = await axios.get("http://localhost:5000/tournament");
-                console.log(res.data);
                 setTournaments(res.data);
+                // Fetch participants for each tournament
+                const participantsPromises = res.data.map(tournament =>
+                    axios.get(`http://localhost:5000/participation/tournament/${tournament.id}`)
+                );
+                const participantsResponses = await Promise.all(participantsPromises);
+                const participantsData = participantsResponses.reduce((acc, res, index) => {
+                    acc[res.data.tournament_id] = res.data;
+                    return acc;
+                }, {});
+                setParticipantsData(participantsData);
             } catch (error) {
                 console.error("Error fetching data:", error);
             }
         };
-        fetchData();
+        fetchTournaments();
     }, []);
+
+    // Fonction pour filtrer les tournois avec inscriptions ouvertes
+    const filterInscriptionsOuvertes = () => {
+        const today = new Date();
+        const year = today.getFullYear();
+        const month = String(today.getMonth() + 1).padStart(2, '0');
+        const day = String(today.getDate()).padStart(2, '0');
+        const formattedDate = `${year}-${month}-${day}`;
+
+        return tournaments.filter(tournament => {
+            const participants = participantsData[tournament.id] || [];
+            return (tournament.total_players > participants.length && tournament.date > formattedDate);
+        });
+    };
+
+    // Fonction pour filtrer les tournois avec inscriptions fermées
+    const filterInscriptionsFermées = () => {
+        const today = new Date();
+        const year = today.getFullYear();
+        const month = String(today.getMonth() + 1).padStart(2, '0');
+        const day = String(today.getDate()).padStart(2, '0');
+        const formattedDate = `${year}-${month}-${day}`;
+
+        return tournaments.filter(tournament => {
+            const participants = participantsData[tournament.id] || [];
+            return !(tournament.total_players > participants.length && tournament.date > formattedDate);
+        });
+    };
 
     return (
         <div>
@@ -37,18 +75,33 @@ export default function Profil() {
                 </div>
             </div>
 
-            {/* Button section */}
+            {/* Current tournament section */}
+            <div className="flex justify-center p-4 mb-4">
+                <button className="relative h-[50px] overflow-hidden bg-opacity-0 px-3 text-white border-l-4 border-primary shadow-2xl transition-all before:absolute before:bottom-0 before:left-0 before:top-0 before:z-0 before:h-full before:w-0 before:bg-primary before:transition-all before:duration-500 hover:text-white hover:shadow-[0_0_20px_rgba(0,0,0,0.8),0_0_20px_var(--tw-shadow-color)] hover:shadow-secondary hover:before:left-0 hover:before:w-full">
+                    <span className="relative z-10">Tournois en cours</span>
+                </button>
+            </div>
+
+            {/* Affichage des tournois en cours (inscriptions ouvertes) */}
+            <div className="flex flex-col items-center p-4">
+                <div className="flex flex-wrap justify-center w-full gap-4">
+                    {filterInscriptionsOuvertes().map((tournament, index) => (
+                        <SmallCards key={tournament.id} tournament={tournament} index={index} />
+                    ))}
+                </div>
+            </div>
+
+            {/* Old tournament section */}
             <div className="flex justify-center p-4 mb-4">
                 <button className="relative h-[50px] overflow-hidden bg-opacity-0 px-3 text-white border-l-4 border-primary shadow-2xl transition-all before:absolute before:bottom-0 before:left-0 before:top-0 before:z-0 before:h-full before:w-0 before:bg-primary before:transition-all before:duration-500 hover:text-white hover:shadow-[0_0_20px_rgba(0,0,0,0.8),0_0_20px_var(--tw-shadow-color)] hover:shadow-secondary hover:before:left-0 hover:before:w-full">
                     <span className="relative z-10">Anciens tournois</span>
                 </button>
             </div>
 
-            {/* New tournament section */}
+            {/* Affichage des anciens tournois (inscriptions fermées) */}
             <div className="flex flex-col items-center p-4">
-                {/* Tournaments section */}
                 <div className="flex flex-wrap justify-center w-full gap-4">
-                    {tournaments.map((tournament, index) => (
+                    {filterInscriptionsFermées().map((tournament, index) => (
                         <SmallCards key={tournament.id} tournament={tournament} index={index} />
                     ))}
                 </div>
