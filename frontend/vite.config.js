28bc88c414aa9cec4f6b906319f003abcbbcc11a
yuuta2s <asmaa.guebba19@gmail.com>
2024-07-10 10:24:18 +0200

    deploiement

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,11 +5,11 @@ import svgr from 'vite-plugin-svgr';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  build: {
-    rollupOptions: {
-      external: ['build.rollupOptions.external'] // Remplacez 'module-name' par le nom du module à externaliser
-    }
-  },
+  // build: {
+  //   rollupOptions: {
+  //     external: ['build.rollupOptions.external'] // Remplacez 'module-name' par le nom du module à externaliser
+  //   }
+  // },
   plugins: [react(),svgr()],
   resolve: {
     alias: {
@@ -17,7 +17,6 @@ export default defineConfig({
       "@components": path.resolve(__dirname, "src/components"),
       "@pages": path.resolve(__dirname, "src/pages"),
       "@services": path.resolve(__dirname, "src/services"),
-      '@': path.resolve(__dirname, './src'),
     },
   },
   assetsInclude: ['**/*.PNG'],
