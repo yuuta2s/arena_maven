7be4e7def22748529173ddde841b313f3ef27757
yuuta2s <asmaa.guebba19@gmail.com>
2024-07-10 11:09:26 +0200

    installed rollup

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,16 +1,27 @@
 const path = require("path");
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import svgr from 'vite-plugin-svgr';
+import resolve from "@rollup/plugin-node-resolve"; // Plugin pour résoudre les dépendances des modules
+import commonjs from "@rollup/plugin-commonjs"; // Plugin pour convertir les modules CommonJS en ES6
 
 // https://vitejs.dev/config/
 export default defineConfig({
   build: {
     rollupOptions: {
-      external: ['build.rollupOptions.external'] // Remplacez 'module-name' par le nom du module à externaliser
-    }
+      external: ["build.rollupOptions.external"], // Remplacez 'module-name' par le nom du module à externaliser
+    },
   },
-  plugins: [react(),svgr()],
+  plugins: [
+    react(),
+    resolve(), // Résout les dépendances des modules
+    commonjs(), // Convertit les modules CommonJS en ES6
+  ],
+  input: "src/main.js", // Point d'entrée de votre application
+  output: {
+    file: "dist/bundle.js", // Fichier de sortie après le bundling
+    format: "es", // Format de module (ES6)
+    sourcemap: true, // Générer les sourcemaps pour le débogage
+  },
   resolve: {
     alias: {
       "@assets": path.resolve(__dirname, "src/assets"),
@@ -19,5 +30,5 @@ export default defineConfig({
       "@services": path.resolve(__dirname, "src/services"),
     },
   },
-  assetsInclude: ['**/*.PNG'],
+  assetsInclude: ["**/*.PNG"],
 });
