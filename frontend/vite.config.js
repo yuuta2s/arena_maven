10e6c74c64afb88fd5138b8f60d6de6b016a3ed8
yuuta2s <asmaa.guebba19@gmail.com>
2024-07-10 09:50:24 +0200

    fixing vite config

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,6 +4,11 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  build: {
+    rollupOptions: {
+      external: ['build.rollupOptions.external'] // Remplacez 'module-name' par le nom du module à externaliser
+    }
+  },
   plugins: [react()],
   resolve: {
     alias: {
