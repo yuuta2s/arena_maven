04afd3e9fdbdd1742605e5555bce289e6dce4afb
ElioDKM <eliodikoume@gmail.com>
2024-07-18 16:26:06 +0200

    responsive modal et homepage

--- frontend/src/components/Homepage/Homepage.jsx
+++ frontend/src/components/Homepage/Homepage.jsx
@@ -4,7 +4,7 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import { Link } from 'react-router-dom';
-import img1 from '../../../public/images/peakpx.jpg';
+import img1 from '../../assets/peakpx.jpg';
 
 import SmallCards from '../SmallCards/SmallCards';
 
@@ -26,12 +26,9 @@ export default function Homepage() {
 
   return (
     <div className="p-4">
-      <div className="flex flex-col items-center mb-24 bg-underline-title bg-no-repeat bg-bottom pb-14">
-        <h1 className="text-7xl font-bold text-white">Become an Arena Maven</h1>
-      </div>
-      <section className="flex justify-between items-center bg-black rounded-lg bg-opacity-40 p-6 max-w-7xl h-128 mx-auto">
+      <section className="flex flex-wrap justify-center items-center bg-black rounded-lg bg-opacity-40 p-6 max-w-7xl  mx-auto">
         <div className="p-3 max-w-3xl">
-          <h2 className="text-4xl font-bold text-white p-5">To be or not to be? That is the question!</h2>
+          <h1 className="text-3xl sm:text-4xl md:text-5xl lg:text-7xl font-bold text-white">Become an <br /> Arena Maven</h1>
           <p className="text-white text-xl p-5">Bienvenue sur Arena Maven, la plateforme ultime pour créer et participer aux tournois de Tekken 8. Rejoignez la communauté des champions et montrez vos compétences dans l'arène!</p>
           <div className="flex flex-wrap justify-between items-center gap-3 p-5">
             <Link to="/tournamentRequest">
@@ -46,14 +43,13 @@ export default function Homepage() {
             </Link>
           </div>
         </div>
-        <div className="w-full lg:w-1/2 h-64 lg:h-auto mt-6 lg:mt-0">
+        <div className="max-w-md lg:w-1/2 h-64 lg:h-auto mt-6 lg:mt-0">
           <img src={img1} alt="Arena Maven" className="w-full h-full object-cover rounded-lg" />
         </div>
       </section>
       <section className="max-w-7xl mx-auto">
         <article className="mt-8">
 
-          <h2 className="text-3xl text-white">Novelty:</h2>
           <h2 className="text-3xl text-white">Nouveautés :</h2>
           {tournaments.length > 0 && (
             <div className="flex flex-wrap items-center justify-evenly">
@@ -64,7 +60,6 @@ export default function Homepage() {
           )}
         </article>
         <article className="mt-8">
-          <h2 className="text-3xl text-white">Last Chance:</h2>
           <h2 className="text-3xl text-white">Dernières chances :</h2>
           {tournaments.length > 0 && (
             <div className="flex flex-wrap items-center justify-evenly">
